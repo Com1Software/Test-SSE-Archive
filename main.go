63e90cddc0587f4d26a83accbb9d73680ef9da22
Lenovo 15 <information@com1software.com>
2022-09-24 16:20:06 -0400

    update

--- main.go
+++ main.go
@@ -2,13 +2,10 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"net/http"
 	"os/exec"
 	"runtime"
-	"strings"
-
-	"go.bug.st/serial"
+	"time"
 )
 
 //-------------------------------------------------------------------------
@@ -17,121 +14,17 @@ func main() {
 	fmt.Println("Test Server Sent Events ")
 	fmt.Printf("Operating System : %s\n", runtime.GOOS)
 
-	ports, err := serial.GetPortsList()
-	if err != nil {
-		log.Fatal(err)
-	}
-	if len(ports) == 0 {
-		log.Fatal("No serial ports found!")
-	}
-
-	// Print the list of detected ports
-	for _, port := range ports {
-		fmt.Printf("Found port: %v\n", port)
-	}
-
-	// Open the first serial port detected at 9600bps N81
-	mode := &serial.Mode{
-		BaudRate: 4800,
-		Parity:   serial.NoParity,
-		DataBits: 8,
-		StopBits: serial.OneStopBit,
-	}
-	port, err := serial.Open(ports[0], mode)
-	if err != nil {
-		log.Fatal(err)
-	}
-
 	go func() {
 		for {
-			line := ""
-			buff := make([]byte, 1)
-			on := true
-			for on != false {
-				line = ""
-				for {
-					n, err := port.Read(buff)
-					if err != nil {
-						log.Fatal(err)
-					}
-					if n == 0 {
-						fmt.Println("\nEOF")
-						break
-					}
-					line = line + string(buff[:n])
-					if strings.Contains(string(buff[:n]), "\n") {
-						break
-					}
-
-				}
-				id, latitude, longitude, ns, ew, speed, degree := getGPSPosition(line)
-				if len(id) > 0 {
-					// fmt.Println(line)
-					event := fmt.Sprintf("%s  latitude=%s  %s  longitude=%s %s  knots=%s degrees=%s\n", id, latitude, ns, longitude, ew, speed, degree)
-					fmt.Println(event)
-					agent.Notifier <- []byte(event)
-				}
-			}
+			time.Sleep(time.Second * 2)
+			event := fmt.Sprintf(" %v\n", time.Now())
+			agent.Notifier <- []byte(event)
 		}
 	}()
 
 	Openbrowser("http://localhost:8080")
-	log.Fatal("HTTP server error: ", http.ListenAndServe("localhost:8080", agent))
+	http.ListenAndServe("localhost:8080", agent)
 
-}
-
-func getGPSPosition(sentence string) (string, string, string, string, string, string, string) {
-	data := strings.Split(sentence, ",")
-	id := ""
-	latitude := ""
-	longitude := ""
-	ns := ""
-	ew := ""
-	speed := ""
-	degree := ""
-
-	switch {
-	case string(data[0]) == "$GPGGA":
-		id = data[0]
-		latitude = data[2]
-		ns = data[3]
-		longitude = data[4]
-		ew = data[5]
-	case string(data[0]) == "$GPGLL":
-		id = data[0]
-		latitude = data[1]
-		ns = data[2]
-		longitude = data[3]
-		ew = data[4]
-
-	case string(data[0]) == "$GPVTG":
-		//id = data[0]
-		degree = data[1]
-
-	case string(data[0]) == "$GPRMC":
-		id = data[0]
-		latitude = data[3]
-		ns = data[4]
-		longitude = data[5]
-		ew = data[6]
-		speed = data[7]
-		degree = data[8]
-
-	case string(data[0]) == "$GPGSA":
-		id = data[0]
-
-	case string(data[0]) == "$GPGSV":
-		//id = data[0]
-
-	case string(data[0]) == "$GPTXT":
-		id = data[0]
-
-	default:
-		fmt.Println("-- %s", data[0])
-
-	}
-
-	return id, latitude, longitude, ns, ew, speed, degree
 }
 
 // Openbrowser : Opens default web browser to specified url
